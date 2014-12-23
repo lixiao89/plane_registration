fc11d83d3f37135bf8248e4de4b5ac8595b2f422
xli4217 <xli87@jhu.edu>
2014-12-23 12:21:50 -0500

    update

--- plane_registration/src/wam_move.cpp
+++ plane_registration/src/wam_move.cpp
@@ -9,132 +9,158 @@
 #include <sensor_msgs/Joy.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/WrenchStamped.h>
+#include <geometry_msgs/Vector3.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
 #include <kdl_conversions/kdl_msg.h>
+#include <std_msgs/Int32.h>
 
 #include <math.h>   // PI
 
 #define PI 3.1415926
 
-// TODO
-//  - add joint pos callback
-//  - test different types of ik solvers
+    // TODO
+    //  - add joint pos callback
+    //  - test different types of ik solvers
 
 
-// Steps :
-//  - r: set WAM to ready state
-//  - h: start hybrid controller
+    // Steps :
+    //  - r: set WAM to ready state
+    //  - h: start hybrid controller
 
 
 #define DEBUG 0
 
-using namespace KDL;
+    using namespace KDL;
 
-class WamMove
-{
-public:
-  enum CONTROL_TYPE
-  {
-    CTRL_IDLE,
-    CTRL_READY,
-    CTRL_ERROR_POSE,
-    CTRL_START_HYBRID,
-    CTRL_HYBRID
-  };
-
-  WamMove(ros::NodeHandle* node):
-    node_(node),
-    ctrl_type_(CTRL_IDLE)
-  {
-    // pubs
-    pub_state_ = node->advertise<std_msgs::String>("/dvrk_psm1/set_robot_state", 10);
-    pub_jnt_cmd_ = node->advertise<trajectory_msgs::JointTrajectoryPoint>("/gazebo/traj_rml/joint_traj_point_cmd", 1);
-
-    // subs
-    //sub_joy_ = node->subscribe("/spacenav/joy", 1, &Control::cb_joy, this);
-    sub_jr3_ = node->subscribe("/jr3/wrench", 1, &WamMove::cb_jr3, this);
-    sub_key_ = node->subscribe("/hybrid/key", 1, &WamMove::cb_key, this);
-    sub_states_ = node->subscribe("/gazebo/barrett_manager/wam/joint_states", 1, &WamMove::cb_jnt_states, this);
-
-    // class memeber
-    init_kdl_robot();
-  }
-
-  virtual ~WamMove(){}
-
-  void update()
-  {
-    // do some update here
-    // MODE
-    if (ctrl_type_ == CTRL_HYBRID || ctrl_type_ == CTRL_START_HYBRID)
+    class WamMove
     {
-//      ROS_INFO("in hybrid control");
-      // compute cmd frame
-//      static KDL::Vector inc(0, 0, 0.001);
-//      cmd_frame_.p += inc;
+    public:
+      enum CONTROL_TYPE
+      {
+        CTRL_IDLE,
+        CTRL_READY,
+        CTRL_ERROR_POSE,
+        CTRL_START_HYBRID,
+        CTRL_HYBRID
+      };
 
-//      if (cmd_frame_.p.z() < 0.6) {
-//        inc = KDL::Vector(0, 0, 0.001);
-//      }
-//      else if (cmd_frame_.p.z() > 0.8) {
-//        inc = KDL::Vector(0, 0, -0.001);
-//      }
+      WamMove(ros::NodeHandle* node):
+        node_(node),
+        ctrl_type_(CTRL_IDLE)
+      {
+        // pubs
+        pub_state_ = node->advertise<std_msgs::String>("/dvrk_psm1/set_robot_state", 10);
+        pub_jnt_cmd_ = node->advertise<trajectory_msgs::JointTrajectoryPoint>("/gazebo/traj_rml/joint_traj_point_cmd", 1);
+        pub_plane_reg_pts_ = node->advertise<std_msgs::Int32>("/plreg/ptlocation", 10);
+	pub_err_gt_ = node->advertise<geometry_msgs::Vector3>("/plreg/errGroundTruth", 10);
 
-      
+        // subs
+        //sub_joy_ = node->subscribe("/spacenav/joy", 1, &Control::cb_joy, this);
+        sub_jr3_ = node->subscribe("/jr3/wrench", 1, &WamMove::cb_jr3, this);
+        sub_key_ = node->subscribe("/hybrid/key", 1, &WamMove::cb_key, this);
+        sub_states_ = node->subscribe("/gazebo/barrett_manager/wam/joint_states", 1, &WamMove::cb_jnt_states, this);
+
+        // class memeber
+        init_kdl_robot();
+
+        last_hpf_cmd = 0;
+	err_gt.x = 0;
+	err_gt.y = 0;
+	err_gt.z = 0;
+      }
+
+      virtual ~WamMove(){}
+
+      void update()
+      {
+        // do some update here
+	
+	// publish orientation error ground truth
+	pub_err_gt_.publish( err_gt );
+
+        // MODE
+        if (ctrl_type_ == CTRL_HYBRID || ctrl_type_ == CTRL_START_HYBRID)
+        {
+    //      ROS_INFO("in hybrid control");
+          // compute cmd frame
+    //      static KDL::Vector inc(0, 0, 0.001);
+    //      cmd_frame_.p += inc;
+
+    //      if (cmd_frame_.p.z() < 0.6) {
+    //        inc = KDL::Vector(0, 0, 0.001);
+    //      }
+    //      else if (cmd_frame_.p.z() > 0.8) {
+    //        inc = KDL::Vector(0, 0, -0.001);
+    //      }
+
+          
 #if 1
-      Vector eevel;
-      Vector eerot;
-      // if( ctrl_type_ == CTRL_START_HYBRID )
-      // 	{
-      // 	  Vector eevel( 0, 0, 0 );
-      // 	  Vector eerot( 0, 0, 0 );
-      // 	}
-      if( ctrl_type_ == CTRL_HYBRID )
-	{
-	  eevel.x( 0.5 );
-	}
+          Vector eevel;
+          Vector eerot;
+          // if( ctrl_type_ == CTRL_START_HYBRID )
+          // 	{
+          // 	  Vector eevel( 0, 0, 0 );
+          // 	  Vector eerot( 0, 0, 0 );
+          // 	}
+          if( ctrl_type_ == CTRL_HYBRID )
+        {
+          eevel.z( -0.01 );
+        }
 
-      double vel_gain = 0.1;
-      double rot_gain = 0.4;
-      //Twist cmd_twist = twist_;
-      Twist cmd_twist;
-      cmd_twist.vel = vel_gain * eevel;
-      cmd_twist.rot = rot_gain * eerot;
+          double vel_gain = 0.1;
+          double rot_gain = 0.4;
+          //Twist cmd_twist = twist_;
+          Twist cmd_twist;
+          cmd_twist.vel = vel_gain * eevel;
+          cmd_twist.rot = rot_gain * eerot;
 
-      // ignore some axes
-      cmd_twist.vel(0) = 0.0;  // ignore x
-      cmd_twist.rot(1) = 0.0;  // ignore y
-      cmd_twist.rot(2) = 0.0;  // ignore z
-
-      cmd_twist = cmd_frame_.M.Inverse(cmd_twist);
-      cmd_frame_.Integrate(cmd_twist, 50);
+          // ignore some axes
+          cmd_twist.vel(0) = 0.0;  // ignore x
+          cmd_twist.rot(1) = 0.0;  // ignore y
+          cmd_twist.rot(2) = 0.0;  // ignore z
+          //cmd_twist.rot(0) = 0.0; //ignore x
+        
+          cmd_twist = cmd_frame_.M.Inverse(cmd_twist);
+          cmd_frame_.Integrate(cmd_twist, 50);
 #endif
 
 
-      // force control
-      double cmd_force = -4.0;
-      double err_force = cmd_force - jr3_wrench_.force.z();
-      double cmd_v = 0.0;
+          // force control
+          double cmd_force = -4.0;
+          double err_force = cmd_force - jr3_wrench_.force.z();
+          double cmd_v = 0.0;
 
-      cmd_v = -0.0001 * err_force;
-      if (cmd_v > 0.0001) cmd_v = 0.0001;
-      if (cmd_v < -0.0001) cmd_v = -0.0001;
-      if (fabs(cmd_v) < 0.00005) cmd_v = 0.0;
+          cmd_v = -0.0001 * err_force;
+          if (cmd_v > 0.0001) cmd_v = 0.0001;
+          if (cmd_v < -0.0001) cmd_v = -0.0001;
+          if (fabs(cmd_v) < 0.00005) cmd_v = 0.0;
 
-      Frame force_frame;
-      force_frame.p(2) = cmd_v;
-      cmd_frame_ = cmd_frame_ * force_frame;
+          //------ for plane registration -------------
+          std_msgs::Int32 plreg_msg;
+          if( last_hpf_cmd > 0 || cmd_v < 0){
+              plreg_msg.data = 1;
+          }
+          else
+          {
+              plreg_msg.data = 0;
+          }
+            
+          pub_plane_reg_pts_.publish( plreg_msg );
+          //----------------------------
+          Frame force_frame;
+          force_frame.p(2) = cmd_v;
+          cmd_frame_ = cmd_frame_ * force_frame;
 
-      // do ik
-      ik_solver_->CartToJnt(jnt_pos_, cmd_frame_, jnt_cmd_);
-      std::cout << "cmd: " << jnt_cmd_.data.transpose() << std::endl;
+          // do ik
+          ik_solver_->CartToJnt(jnt_pos_, cmd_frame_, jnt_cmd_);
+	  // std::cout << "cmd: " << jnt_cmd_.data.transpose() << std::endl;
 
-      // send to robot
-      trajectory_msgs::JointTrajectoryPoint msg_jnt_cmd;
-      msg_jnt_cmd.positions.resize(num_jnts_);
-      for (size_t i = 0; i < num_jnts_; i++)
-      {
-        msg_jnt_cmd.positions[i] = jnt_cmd_(i);
+          // send to robot
+          trajectory_msgs::JointTrajectoryPoint msg_jnt_cmd;
+          msg_jnt_cmd.positions.resize(num_jnts_);
+          for (size_t i = 0; i < num_jnts_; i++)
+          {
+            msg_jnt_cmd.positions[i] = jnt_cmd_(i);
       }
       pub_jnt_cmd_.publish(msg_jnt_cmd);
     }
@@ -163,9 +189,9 @@ private:
     ROS_INFO_STREAM("Key = " << msg.data);
     if (msg.data == "r")
       ctrl_ready();
-    if (msg.data == "ep")
+    else if (msg.data == "ep")
       ctrl_errorpos();
-    if (msg.data == "sh")
+    else if (msg.data == "sh")
       ctrl_start_hybrid();
     else if (msg.data == "h")
       ctrl_hybrid();
@@ -273,8 +299,8 @@ private:
     cmd_frame_ = tip_frame_;
 
     double xerr = 0*PI/180;
-    double yerr = 15*PI/180;
-    double zerr = 0;
+    double yerr = 20*PI/180;
+    double zerr = 0*PI/180;
 
     Rotation roterr;
     roterr.DoRotX( xerr );
@@ -297,6 +323,21 @@ private:
         msg_jnt_cmd.positions[i] = jnt_cmd_(i);
       }   
     pub_jnt_cmd_.publish(msg_jnt_cmd);
+
+    // unit vector of x, y, z directions of tip frame
+    Vector cutterX_ori = cmd_frame_.M.UnitX();
+    Vector cutterY_ori = cmd_frame_.M.UnitY();
+    Vector cutterZ_ori = cmd_frame_.M.UnitZ();
+
+
+    //std::cout<<"tip frame: "<<tip_frame_<<std::endl;
+    std::cout<<"x vector: "<<cutterX_ori<<std::endl;
+    std::cout<<"y vector: "<<cutterY_ori<<std::endl;
+    std::cout<<"z vector: "<<cutterZ_ori<<std::endl;
+
+    err_gt.x = 0;
+    err_gt.y = atan2( cutterX_ori[0], cutterX_ori[2] )*180/PI;
+    err_gt.z = 0;
   }     
 
 
@@ -325,6 +366,8 @@ private:
   ros::NodeHandle* node_;
   ros::Publisher pub_state_;
   ros::Publisher pub_jnt_cmd_;
+  ros::Publisher pub_plane_reg_pts_;
+  ros::Publisher pub_err_gt_;
   ros::Subscriber sub_joy_;
   ros::Subscriber sub_jr3_;
   ros::Subscriber sub_key_;
@@ -358,6 +401,11 @@ private:
 
   // ctrl mode
   CONTROL_TYPE ctrl_type_;
+
+  // --------- for plane registration ----------------
+  std_msgs::Int32 plreg_pts;
+  double last_hpf_cmd;
+  geometry_msgs::Vector3 err_gt;
 };
 
 
